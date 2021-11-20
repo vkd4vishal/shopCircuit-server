4323bde01f548c56ad266e4492ed25e8736fef2a
Vishal Kumar <vishalkd0066@gmail.com>
2021-11-20 12:03:06 +0530

    feat(backend): added authenticate middleware

--- src/apis/routes/routes.ts
+++ src/apis/routes/routes.ts
@@ -1,7 +1,7 @@
 import express from 'express'
-import { signUp, updateUserImage, validateUser, validateFile, getProfile, deleteUser,updateUserProfile,login } from '../controllers'
-import { upload, userProfileSchema, userImageReqSchema ,updateUserProfileSchema,userLoginSchema} from '../validators'
-import { validateBody, validateHeaders } from '../../utils/reqValidator';
+import { signUp, updateUserImage, validateUser, validateFile, getProfile, deleteUser, updateUserProfile, login } from '../controllers'
+import { upload, userProfileSchema, userImageReqSchema, updateUserProfileSchema, userLoginSchema } from '../validators'
+import { validateBody, validateHeaders, auth } from '../../utils';
 
 export const router = express.Router();
 
@@ -9,11 +9,11 @@ export const router = express.Router();
 router.post('/signup', validateBody(userProfileSchema), signUp);
 router.post('/updateUserImage',
     validateHeaders(userImageReqSchema), validateUser, upload.single('photo'), validateFile, updateUserImage);
-router.get('/getProfile', validateHeaders(userImageReqSchema), validateUser, getProfile)
+router.get('/getProfile', auth, validateHeaders(userImageReqSchema), validateUser, getProfile)
 router.delete('/deleteUser', validateHeaders(userImageReqSchema), validateUser, deleteUser)
 
-router.put('/updateUserProfile',validateHeaders(userImageReqSchema), validateBody(updateUserProfileSchema),validateUser, updateUserProfile);
-router.put('/login',validateBody(userLoginSchema), login);
+router.put('/updateUserProfile', validateHeaders(userImageReqSchema), validateBody(updateUserProfileSchema), validateUser, updateUserProfile);
+router.put('/login', validateBody(userLoginSchema), login);
 
 
 
---
+++ src/utils/authenticate.ts
@@ -0,0 +1,25 @@
+import { Request, Response, RequestHandler, NextFunction } from "express";
+
+import jwt from "jsonwebtoken"
+import { sendError } from "./index";
+
+export const auth = (req: Request, res: Response, next: NextFunction) => {
+
+    const token = req.header("token");
+    if (!token) {
+        return sendError(res, 401, "Auth Error")
+
+    }
+
+    try {
+        const decoded: any = jwt.verify(token, process.env.JWT_SECRET ?? "");
+        if (decoded.user.id !== req.headers.userid) {
+            console.log(decoded.user.id, req.headers.userid)
+            return sendError(res, 500, "Unauthorized User.")
+
+        }
+        next();
+    } catch (e) {
+        return sendError(res, 500, "Invalid token")
+    }
+};
